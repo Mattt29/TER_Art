bd51fa9cdf4d056382fc75b1b6a55cd58b7971c4
nihil0r <brice.grivet.1995@gmail.com>
2023-04-27 23:50:41 +0200

    phase page Your exploration

--- Codes/Site/Website_V2/static/js/script.js
+++ Codes/Site/Website_V2/static/js/script.js
@@ -20,7 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     toggler.addEventListener("click", toggleNav);
 
-
     new ResizeObserver(entries => {
       if (entries[0].contentRect.width <= 900) {
         navLinksContainer.style.transition = "transform 0.4s ease-out";
@@ -29,44 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }).observe(document.body)
 
-
-    const navLinks = document.querySelectorAll('.navlinks-container a');
-
-    navLinks.forEach((link) => {
-      link.addEventListener('mouseenter', (e) => {
-        const target = e.target;
-        const letters = target.textContent.split('');
-        target.textContent = '';
-        let delay = 0;
-
-        letters.forEach((letter, index) => {
-          const span = document.createElement('span');
-          span.textContent = letter;
-          span.style.animationDelay = `${delay}ms`;
-          target.appendChild(span);
-          delay += 100;
-        });
-      });
-
-      link.addEventListener('mouseleave', (e) => {
-        let target = e.target;
-        if (!(target instanceof Element)) {
-          return;
-        }
-
-        while (target.tagName !== 'A' && target.parentElement) {
-          target = target.parentElement;
-        }
-
-        const spans = target.querySelectorAll('span');
-
-        spans.forEach((span, index) => {
-          setTimeout(() => {
-            span.style.color = 'rgba(65, 65, 65, 1)';
-          }, index * 100);
-        });
-      });
-    });
   }
 
   // HOME.HTML
@@ -218,7 +179,7 @@ document.addEventListener("DOMContentLoaded", async function () {
           setTimeout(() => {
             introAnimation.style.display = "none";
             resolve(); // Résoudre la promesse pour indiquer que la vérification est terminée
-          }, 3000);
+          }, 4000);
         }, 3000);
       } else {
         introAnimation.style.display = "none";
